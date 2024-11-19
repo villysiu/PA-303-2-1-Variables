7a1d9b72855fda9b87d3e4cb0d67362d8f79d4cf
Villy Siu <villysiu@gmail.com>
2024-11-19 06:24:28 -0800

    updated wrapper class to primitive

--- src/JavaBasicsClass.java
+++ src/JavaBasicsClass.java
@@ -2,14 +2,14 @@
 public class JavaBasicsClass {
     public static void main(String[] args) {
         JavaMethods obj = new JavaMethods();
-//        obj.addIntegers();
-//        obj.addDoubles();
-//        obj.addIntAnfDouble();
-//        obj.divideInteger();
-//        obj.divideDouble();
+        obj.addIntegers();
+        obj.addDoubles();
+        obj.addIntAnfDouble();
+        obj.divideInteger();
+        obj.divideDouble();
         obj.divideIntAndDouble();
-//        obj.getCircleArea();
-//        obj.cafe();
+        obj.getCircleArea();
+        obj.cafe();
     }
 
 }
