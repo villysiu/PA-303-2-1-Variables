b84a46a3e918fbc0faf89b61a3696ee9408de56c
Villy Siu <villysiu@gmail.com>
2024-11-19 06:19:00 -0800

    updated wrapper class to primitive

--- src/JavaBasicsClass.java
+++ src/JavaBasicsClass.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
+//package com.perscholas.java_basics;
 public class JavaBasicsClass {
+    public static void main(String[] args) {
+        JavaMethods obj = new JavaMethods();
+//        obj.addIntegers();
+//        obj.addDoubles();
+//        obj.addIntAnfDouble();
+//        obj.divideInteger();
+//        obj.divideDouble();
+        obj.divideIntAndDouble();
+//        obj.getCircleArea();
+//        obj.cafe();
+    }
+
 }
--- src/JavaMethods.java
+++ src/JavaMethods.java
@@ -1,6 +1,6 @@
 //package com.perscholas.java_basics;
 
-public class Something {
+public class JavaMethods {
 
     public void addIntegers(){
         int x=6, y=3;
@@ -12,19 +12,19 @@ public class Something {
         System.out.println(x + " + " + y + " = " + (x + y));
     }
     public void addIntAnfDouble(){
-        double x=6.88;
-        int y =3;
-        Object res = null;
-        res=x+y;
+//        double x=6.88;
+//        int y =3;
+        Double x=6.88;
+        Integer y =3;
+        Object res = x+y;
         System.out.println(x + " + " + y + " = " + res);
-        System.out.println("The variable type of adding integer and douible is " + res.getClass());
+        System.out.println("The variable type of adding integer and double is " + res.getClass());
     }
     public void divideInteger(){
         int x=6, y=30;
         double doubleY = 30.68;
 
-        Object res = null;
-        res= y / x;
+        Object res = y / x;
         System.out.println(y + " / " + x + " = " + res);
         System.out.println("The variable type of result is " + res.getClass());
 
@@ -36,12 +36,11 @@ public class Something {
     public void divideDouble(){
         double y = 30.68, x=6.3;
 
-        Object res = null;
-        res= y / x;
+        Object res = y / x;
         System.out.println(y + " / " + x + " = " + res);
         System.out.println("The variable type of res is " + res.getClass());
 
-        res = (int)(y/x);
+        res = (int)(res);
         System.out.println("(int)("+y + " / " + x + ") = " + res);
         System.out.println("The variable type of casting res to integer is " + res.getClass());
 
@@ -49,11 +48,9 @@ public class Something {
     public void divideIntAndDouble(){
         int x=5, y=6;
         Object q = y / x;
-
-//        Double m = Double.valueOf(y);
         System.out.println(y + " / " + x + " = " + q);
-        Double qq = Double.valueOf(y);
-        System.out.println("Double.valueOf(" + y+ ") = " + qq);
+        q = (double) y;
+        System.out.println("Double.valueOf(" + y+ ") = " + q);
 
     }
     public void getCircleArea(){
@@ -64,4 +61,24 @@ public class Something {
         System.out.println("area of circle with 5cm radius is "+ area );
 
     }
+    public void cafe(){
+        final double saleTax = 10.2;
+        double subtotal = 0.0;
+        double totalSale = 0.0;
+
+        double coffee = 5.0;
+        double espresso = 7.0;
+        double greenTea =6.5;
+
+        subtotal = coffee * 3 + espresso * 4 + greenTea * 2;
+
+        totalSale = subtotal + (subtotal * saleTax / 100);
+        System.out.println("3 coffee $5 + 4 espresso $7 + 2 green tea $6.5 = " + subtotal );
+        System.out.println("subtotal + sale tax = " + totalSale );
+
+
+    }
+
+
+
 }
